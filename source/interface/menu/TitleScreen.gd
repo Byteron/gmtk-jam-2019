8b3aaa1b4959c43ab1d76ee0271e4d5b36400a45
Aaron Winter <winter.aaron93@gmail.com>
2019-08-02 16:16:31 +0200

    added scene_manager plugin, which is a plugin version of the Scene.tscn which we used in previous games

--- source/interface/menu/TitleScreen.gd
+++ source/interface/menu/TitleScreen.gd
@@ -1,1 +1,10 @@
 extends Control
+
+func _ready() -> void:
+	Scene.register_scene("Game", "res://source/game/Game.tscn")
+
+func _on_Play_pressed() -> void:
+	Scene.change("Game", true)
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
