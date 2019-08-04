ea5d0a58df96c640d5d983ed598586ff77e677a6
Aaron Winter <winter.aaron93@gmail.com>
2019-08-04 20:21:07 +0200

    fix flipping

--- source/character/player/Player.gd
+++ source/character/player/Player.gd
@@ -1,11 +1,22 @@
 extends Character
 class_name Player
 
+const body_down_tex := preload("res://graphics/characters/player/mc-body-forward.png")
+const body_side_tex := preload("res://graphics/characters/player/mc-body-side.png")
+const body_up_tex := preload("res://graphics/characters/player/mc-body-forward.png")
+
+const legs_down_tex := preload("res://graphics/characters/player/mc-legs-forward.png")
+const legs_side_tex := preload("res://graphics/characters/player/mc-legs-side.png")
+const legs_up_tex := preload("res://graphics/characters/player/mc-legs-forward.png")
+
 var current_animation := ""
 var facing := Vector2(1, 0)
 
 onready var anim_upper := $Upper/AnimationPlayer as AnimationPlayer
 onready var anim_lower := $Lower/AnimationPlayer as AnimationPlayer
+
+onready var sprite_upper := $Upper/Sprite
+onready var sprite_lower := $Lower/Sprite
 
 onready var tween := $Tween as Tween
 
@@ -21,6 +32,35 @@ func _process(delta: float) -> void:
 
 	if input_direction:
 		facing = input_direction
+
+	if facing.x == 1:
+		flip_right()
+	if facing.x == -1:
+		flip_left()
+
+	var anim_direction = get_anim_direction()
+
+	match anim_direction:
+		Vector2.UP:
+			sprite_upper.texture = body_up_tex
+			sprite_lower.texture = legs_up_tex
+		Vector2.DOWN:
+			sprite_upper.texture = body_down_tex
+			sprite_lower.texture = legs_down_tex
+		Vector2.LEFT:
+			sprite_upper.texture = body_side_tex
+			sprite_lower.texture = legs_side_tex
+		Vector2.RIGHT:
+			sprite_upper.texture = body_side_tex
+			sprite_lower.texture = legs_side_tex
+
+func flip_left():
+	sprite_lower.flip_h = true
+	sprite_upper.flip_h = true
+
+func flip_right():
+	sprite_lower.flip_h = false
+	sprite_upper.flip_h = false
 
 func play_footstep() -> void:
 	Sounds.play_sfx("Footstep")
@@ -38,6 +78,16 @@ func play_lower(anim_name: String) -> void:
 		return
 
 	anim_lower.play(anim_name)
+
+func get_anim_direction() -> Vector2:
+	var direction = get_input_direction()
+
+	if facing.y:
+		direction.x = 0
+	elif facing.x:
+		direction.y = 0
+
+	return direction
 
 func get_input_direction() -> Vector2:
 
